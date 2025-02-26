0e9ecac8c0c5c771c822ac49b30855f4a9e568a6
tiaprime <tiaprime@byu.edu>
2025-02-26 10:25:24 -0700

    Add project files.

---
+++ Mission008_Team0107/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission008_Team0107.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
