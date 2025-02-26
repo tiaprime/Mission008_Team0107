dcb60de6ac8a62f45bd5be3cb7e1bcff0bbb01a0
amymmcd <amymargaret22@gmail.com>
2025-02-26 15:08:58 -0700

    First model commit

---
+++ Mission008_Team0107/Models/Category.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission008_Team0107.Models;
+
+public class Category
+{
+    [Key]
+    public int CategoryId { get; set; }
+    [Required]
+    public string CategoryName { get; set; }
+}
---
+++ Mission008_Team0107/Models/Quadrant.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Mission008_Team0107.Models;
+
+public class Quadrant
+{
+    [Key]
+    public int QuadrantId { get; set; }
+    [Required]
+    public string QuadrantName { get; set; }
+}
